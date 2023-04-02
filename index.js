84aa928760ccb507201e8f035bf164d19dc2288b
Sharonne24 <sawuor395@gmail.com>
2023-04-03 00:13:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Sharon Awuor';
+
+const welcomeMessage = "Welcome to Flatbook, Sharon Awuor!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, SHARON AWUOR!";
+const upperCaseCurrentUser = currentUser.toUpperCase;
+
+const shortGreeting = "Welcome, S!";
+
+
+
